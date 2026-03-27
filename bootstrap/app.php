e533de965ef2c8ee69fc95d681c0d91f6169fec2
Roberth Silva <pythonocr7@gmail.com>
2026-03-27 10:59:32 -0400

    Adiciona middleware global de headers de seguranca

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,21 +6,19 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        
-        // --- AQUI ESTÁ A CORREÇÃO ---
-        // Estamos registrando o apelido 'super.admin' para o Laravel encontrar sua classe
+        $middleware->append(\App\Http\Middleware\SecurityHeaders::class);
+
         $middleware->alias([
             'super.admin' => \App\Http\Middleware\CheckSuperAdmin::class,
             'role' => \App\Http\Middleware\CheckRole::class,
             'verified_custom' => \App\Http\Middleware\EnsureUserIsVerified::class,
             'local_admin.primeiro_acesso' => \App\Http\Middleware\GarantirTrocaSenhaPrimeiroAcessoAdminLocal::class,
         ]);
-
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
