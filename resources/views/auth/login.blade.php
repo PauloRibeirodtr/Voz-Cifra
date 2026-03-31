111d9512338d9af3cd25088f7558d5c4284e0da8
Roberth Silva <pythonocr7@gmail.com>
2026-03-31 13:15:43 -0400

    Adiciona favicon com logo da aplicacao

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login | Ministerio Musical</title>
+    <link rel="icon" type="image/png" href="{{ asset('logo/final.png') }}">
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
